febcfbafdef52b325aae425376c039f6a343e22c
Yash-Sen <yashsen0402@gmail.com>
2024-07-04 12:05:52 +0530

    All Endpoint Created

--- welldress/urls.py
+++ welldress/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('shop/', views.shop, name='shop'),
+    path('shopingcart/', views.shopingcart, name='shopingcart'),
 ]
--- welldress/views.py
+++ welldress/views.py
@@ -5,3 +5,15 @@ def index(request):
 
 def about(request):
     return render(request, 'website/about.html')
+
+def checkout(request):
+    return render(request, 'website/checkout.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
+
+def shop(request):
+    return render(request, 'website/shop.html')
+
+def shopingcart(request):
+    return render(request, 'website/shopingcart.html')
