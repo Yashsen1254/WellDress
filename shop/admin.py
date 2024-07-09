2d2342afe550db39d53fb6f2b69fc42fe03d8fe0
Yash-Sen <yashsen0402@gmail.com>
2024-07-09 23:12:08 +0530

    admin.py updated

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Role, Category, Product, Wishlist, Offer, Cart,  Order, OrderDetail, Checkout, Review
 
-# Register your models here.
+admin.site.register(Role)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Wishlist)
+admin.site.register(Offer)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(Checkout)
+admin.site.register(Review)
